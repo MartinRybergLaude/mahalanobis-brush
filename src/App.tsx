8ae449a87999f77b3bdde824329e335b793d807e
Martin Ryberg Laude <martin.ryberg.laude@gmail.com>
2025-01-07 14:39:04 +0100

    fix: add min

--- src/App.tsx
+++ src/App.tsx
@@ -43,21 +43,6 @@ function App() {
         <div className="flex gap-4">
           <div className="flex-1">
             <label
-              htmlFor="dims"
-              className="block text-sm font-medium text-gray-700 mb-1"
-            >
-              Dimensions
-            </label>
-            <input
-              type="number"
-              id="dims"
-              name="dimensions"
-              defaultValue={dimensions}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-            />
-          </div>
-          <div className="flex-1">
-            <label
               htmlFor="dataset"
               className="block text-sm font-medium text-gray-700 mb-1"
             >
@@ -76,6 +61,23 @@ function App() {
               <option value="cluster">Sphere</option>
               <option value="donut">Torus</option>
             </select>
+          </div>
+          <div className="flex-1">
+            <label
+              htmlFor="dims"
+              className="block text-sm font-medium text-gray-700 mb-1"
+            >
+              Dimensions
+            </label>
+            <input
+              type="number"
+              id="dims"
+              name="dimensions"
+              defaultValue={dimensions}
+              min="2"
+              max="10"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            />
           </div>
           <div className="flex-1">
             <label
